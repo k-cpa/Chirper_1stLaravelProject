c109df2e1b24bed4c58d7a61e4410a2cd37841ee
Kev <kevin@MacBookPro.lan>
2025-01-28 14:17:57 +0100

    blade+nav+create a relation + migration

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
-    //
+    // Mass assignement protection .. Aller voir la documentation pour comprendre mieux.
+    protected $fillable = [
+        'message',
+    ];
 }
